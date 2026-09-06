703c18cb95e02b38d3db92effa7fbd0579d6b119
kousuke256 <kousukejianyuan@gmail.com>
2026-09-06 17:17:36 +0900

    a

---
+++ hakumon/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField,Header("移動速度")]
+    private float moveSpeed;
+
+    private Rigidbody2D rigid;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigid.linearVelocity = new Vector2(Vector2.left.x*moveSpeed,rigid.linearVelocity.y);
+    }
+}
