eb9ba35f4bb08318c3e6cd3063cd8fe597250c33
harine084 <harinezumi1022ooooo@gmail.com>
2026-09-07 20:25:28 +0900

    1

--- hakumon/Assets/Scripts/Gameover.cs
+++ hakumon/Assets/Scripts/Gameover.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gameover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   private void OnTriggerEnter2D(Collider2D other)  //触れたかをチェック
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.CompareTag("Player"))  //相手がplayerか
+        {
+            SceneManager.LoadScene("GameOver");  //ここにscenename BuildProfileに追加してからやってね
+        }
     }
 }
